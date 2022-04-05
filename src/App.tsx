12ad041efe2bce78ad834d3b5a903b2f4b025ef6
Nguyen Huy Hoang <nguyenhoang080721@gmail.com>
2022-04-05 10:12:39 +0700

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Forgotpassword from './components/Forgotpassword/Forgotpassword'
+import Forgotpassword from './components/Forgotpassword/Forgotpassword';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/forgot' element={<Forgotpassword />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/forgot" element={<Forgotpassword />} />
       </Routes>
     </div>
   );
