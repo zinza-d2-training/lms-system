974e58294951b0b5159bed66978035dc69c39215
Nguyen Huy Hoang <nguyenhoang080721@gmail.com>
2022-04-05 16:16:04 +0700

    implement reset_password

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Forgotpassword from './components/Forgotpassword/Forgotpassword';
+import Resetpassword from './components/Resetpassword/Resetpassword';
+import './App.css'
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/forgot" element={<Forgotpassword />} />
+        <Route path="/reset" element={<Resetpassword />} />
       </Routes>
     </div>
   );
