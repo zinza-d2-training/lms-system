a51b2e41f1df74ac5e425da1a4bd7885f1a9415d
Nguyen Huy Hoang <nguyenhoang080721@gmail.com>
2022-04-06 13:34:49 +0700

    Implement Signup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 import ResetPassword from './components/ResetPassword/ResetPassword';
+import Signup from './components/Signup/Signup';
 import './App.css';
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/reset" element={<ResetPassword />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
